826eca3cc37ac6bb5019ab11f12fa99c13c952c3
Mayuki Sawatari <mayuki@misuzilla.org>
2016-04-28 11:48:13 +0900

    Goodbye TypeScript, Hello ECMAScript.

--- src/viewTgaInGitHub.contentScript.js
+++ src/viewTgaInGitHub.contentScript.js
@@ -1,33 +1,37 @@
-var ViewTgaInGitHub = (function () {
-    function ViewTgaInGitHub() {
-    }
-    ViewTgaInGitHub.main = function () {
+class ViewTgaInGitHub {
+    static main() {
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = chrome.extension.getURL("viewTgaInGitHub.pageScript.js");
         window.document.body.appendChild(script);
-        window.addEventListener('message', function (e) {
+
+
+        window.addEventListener('message', (e) => {
             if (e.data.sender == 'ViewTgaInGitHubBridge' && e.data.event == 'pjax:complete' /* && e.data.origin == 'https://github.com' */) {
                 ViewTgaInGitHub.injectImages();
             }
         });
         ViewTgaInGitHub.injectImages();
-    };
-    ViewTgaInGitHub.injectImages = function () {
+    }
+
+    static injectImages() {
         var isCommitChangeSetPage = window.location.href.match(/\/commit\//) != null;
-        if (!window.location.pathname.endsWith('.tga') && !isCommitChangeSetPage)
-            return;
+        if (!window.location.pathname.endsWith('.tga') && !isCommitChangeSetPage) return;
+
         if (isCommitChangeSetPage) {
             var fileHeaders = document.querySelectorAll('.file-header[data-path$=".tga"]');
             for (var i = 0; i < fileHeaders.length; i++) {
-                var fileHeader = (fileHeaders[i]);
+                var fileHeader = fileHeaders[i];
                 var path = fileHeader.querySelector('a').href;
+
                 // create path to raw image
                 path = path.replace(/\/blob\//, '/raw/') + '?raw=true';
+
                 // create dummy "View Raw" link
                 var viewRawLinkE = document.createElement('a');
                 viewRawLinkE.href = path;
                 viewRawLinkE.textContent = 'View Raw';
+
                 var dataContainer = fileHeader.nextElementSibling;
                 dataContainer.textContent = '';
                 dataContainer.appendChild(viewRawLinkE);
@@ -36,21 +40,25 @@ var ViewTgaInGitHub = (function () {
             }
             console.log(fileHeaders);
         }
+
         var tgaLinks = document.querySelectorAll('.image a[href*=".tga"]');
         for (var i = 0; i < tgaLinks.length; i++) {
             var aElement = tgaLinks[i];
             var url = aElement.href;
+
             aElement.textContent = 'Loading...';
-            (function (aElement) {
+
+            ((aElement) => {
                 var tga = new TGA();
-                tga.open(url, function () {
+                tga.open(url, () => {
                     aElement.textContent = '';
+
                     var canvas = tga.getCanvas();
                     aElement.appendChild(canvas);
                 });
             })(aElement);
         }
-    };
-    return ViewTgaInGitHub;
-})();
+    }
+}
+
 ViewTgaInGitHub.main();
--- src/viewTgaInGitHub.pageScript.js
+++ src/viewTgaInGitHub.pageScript.js
@@ -1,11 +1,9 @@
-var ViewTgaInGitHubBridge = (function () {
-    function ViewTgaInGitHubBridge() {
-    }
-    ViewTgaInGitHubBridge.main = function () {
-        $(document).on('pjax:complete', function (details) {
+class ViewTgaInGitHubBridge {
+    static main() {
+        document.addEventListener('pjax:complete', (details) => {
             window.postMessage({ sender: 'ViewTgaInGitHubBridge', event: 'pjax:complete' }, '*');
         });
-    };
-    return ViewTgaInGitHubBridge;
-})();
+    }
+}
+
 ViewTgaInGitHubBridge.main();
